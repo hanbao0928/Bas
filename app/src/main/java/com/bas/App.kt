1ba03d64bc120f64233ecc4029105c0008480007
Lucio <super_luo@163.com>
2021-09-28 16:50:33 +0800

    update

---
+++ app/src/main/java/com/bas/App.kt
@@ -0,0 +1,89 @@
+package com.bas
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+import cat.ereza.customactivityoncrash.CustomActivityOnCrash
+import cat.ereza.customactivityoncrash.config.CaocConfig
+import com.tencent.bugly.crashreport.CrashReport
+import com.tencent.smtt.export.external.TbsCoreSettings
+import com.tencent.smtt.sdk.QbSdk
+
+/**
+ * Created by Lucio on 2021/9/24.
+ */
+class App : Application() {
+
+    /**
+     * Called when the application is starting, before any activity, service,
+     * or receiver objects (excluding content providers) have been created.
+     *
+     *
+     * Implementations should be as quick as possible (for example using
+     * lazy initialization of state) since the time spent in this function
+     * directly impacts the performance of starting the first activity,
+     * service, or receiver in a process.
+     *
+     *
+     * If you override this method, be sure to call `super.onCreate()`.
+     *
+     *
+     * Be aware that direct boot may also affect callback order on
+     * Android [android.os.Build.VERSION_CODES.N] and later devices.
+     * Until the user unlocks the device, only direct boot aware components are
+     * allowed to run. You should consider that all direct boot unaware
+     * components, including such [android.content.ContentProvider], are
+     * disabled until user unlock happens, especially when component callback
+     * order matters.
+     */
+    override fun onCreate() {
+        super.onCreate()
+
+        val strategy = CrashReport.UserStrategy(this)
+        strategy.isUploadProcess = true
+        CrashReport.initCrashReport(this, "644e75b030", BuildConfig.DEBUG, strategy)
+        CaocConfig.Builder.create()
+            .backgroundMode(CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM) //default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM
+            .enabled(true) //default: true
+            .showErrorDetails(true) //default: true
+            .showRestartButton(true) //default: true
+            .logErrorOnRestart(true) //default: true
+            .trackActivities(false) //default: false
+            .minTimeBetweenCrashesMs(2000) //default: 3000
+//                .errorDrawable(R.drawable.ic_custom_drawable) //default: bug image
+//                .restartActivity(YourCustomActivity::class.java) //default: null (your app's launch activity)
+//                .errorActivity(YourCustomErrorActivity::class.java) //default: null (default error activity)
+//                .eventListener(YourCustomEventListener()) //default: null
+            .eventListener(object : CustomActivityOnCrash.EventListener {
+                override fun onRestartAppFromErrorActivity() {
+
+                }
+
+                override fun onCloseAppFromErrorActivity() {
+                }
+
+                override fun onLaunchErrorActivity() {
+                }
+
+            })
+            .apply()
+
+        val map = HashMap<String, Any>()
+        map[TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER] = true
+        map[TbsCoreSettings.TBS_SETTINGS_USE_DEXLOADER_SERVICE] = true
+        QbSdk.initTbsSettings(map)
+    }
+
+    /**
+     * Set the base context for this ContextWrapper.  All calls will then be
+     * delegated to the base context.  Throws
+     * IllegalStateException if a base context has already been set.
+     *
+     * @param base The new base context for this wrapper.
+     */
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
--- app/src/main/java/com/bas/MainActivity.kt
+++ app/src/main/java/com/bas/MainActivity.kt
@@ -2,14 +2,46 @@ package com.bas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bas.core.android.util.getWifiSSID
-import timber.log.Timber
+import android.os.Handler
+import com.bas.adapter.mediaplayer.ConfigurableVideoView
+import com.bas.adapter.mediaplayer.MediaPlayer
+import com.pili.pldroid.player.widget.PLVideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Timber.tag("Hello").d("Hello World")
-        this.getWifiSSID()
+
+        val url = "http://uxclass.ucuxin.com/uxdev/209/video/21/9/23/2653a0936b7f47a0a2c4c9b389c8e17f?Expires=1632721545&OSSAccessKeyId=LTAI8bWGyBmKUcM8&Signature=iWxkbq4YKS1vkVV%2B2lOJdijYjko%3D"
+        findViewById<ConfigurableVideoView>(R.id.player).also {
+            it.addPlayerListener(object:MediaPlayer.PlayerListener{
+                override fun onPlayPrepared() {
+                    super.onPlayPrepared()
+                    it.start()
+                }
+
+                /**
+                 * 播放结束
+                 */
+                override fun onPlayEnd() {
+                    super.onPlayEnd()
+                    Handler().postDelayed({
+                        findViewById<ConfigurableVideoView>(R.id.player2).also {
+                            it.addPlayerListener(object:MediaPlayer.PlayerListener{
+                                override fun onPlayPrepared() {
+                                    super.onPlayPrepared()
+                                    it.start()
+                                }
+                            })
+                            it.setDataSource(url)
+                        }
+                    },10000)
+                }
+            })
+
+            it.setDataSource("http://highlight-video.cdn.bcebos.com/video/6s/790fb256-1db3-11ec-8b54-6c92bf3b6b4d.mp4?v_from_s=hkapp-haokan-tucheng")
+        }
+
+
     }
 }
