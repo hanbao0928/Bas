5661ad449f4a6726e1fc9bb24a1d1031733c7ee7
SuperLuo <super_luo@163.com>
2022-03-25 16:32:05 +0800

    去除module 'player' 和 'tvarch'

--- player/src/main/java/andme/media/player/core/MediaPlayerException.kt
+++
@@ -1,6 +0,0 @@
-package andme.media.player.core
-
-/**
- * Created by Lucio on 2021/4/21.
- */
-class MediaPlayerException(val what:Int, val extra:Int) : Throwable("what=${what} extra=${extra}")
--- tvarch/src/main/java/com/open/androidtvwidget/keyboard/SoftKeyBoardListener.java
+++
@@ -1,7 +0,0 @@
-package com.open.androidtvwidget.keyboard;
-
-public interface SoftKeyBoardListener {
-	public void onCommitText(SoftKey key);
-	public void onDelete(SoftKey key);
-	public void onBack(SoftKey key);
-}
