c1cf4b24d65874bd2f6fa0ecfb9446cce9a7b8f8
LuoChao <super_luo@163.com>
2022-06-06 22:48:49 +0800

    mac sync

--- droid-net/src/main/java/bas/droid/net/retrofit/ApiServiceFactory.kt
+++ droid-net/src/main/java/bas/droid/net/retrofit/ApiServiceFactory.kt
@@ -1,7 +1,10 @@
 package bas.droid.net.retrofit
 
 import retrofit2.Retrofit
+import kotlin.reflect.KClass
 
 interface ApiServiceFactory {
     val retrofit: Retrofit
+
+    fun <T : Any> createService(clz: KClass<T>): T = retrofit.create(clz.java)
 }
